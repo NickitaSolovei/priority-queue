68a9e4eb9e226d068dc0d25397ec186252053a32
Nickita Solovei <https://github.com/NickitaSolovei>
2019-03-15 22:07:17 +0200

    feat: add queue

--- src/queue.js
+++ src/queue.js
@@ -2,23 +2,32 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+		this.maxSize = maxSize || 30;
+		this.heap = new MaxHeap();
 	}
 
 	push(data, priority) {
-
+		if (this.heap.size() < this.maxSize) { // если меньше maxSize
+			this.heap.push(data, priority);
+		}
+		else { 
+			throw new error;
+		}
 	}
 
 	shift() {
-
+		if (this.heap.parentNodes.length > 0) {
+			return this.heap.pop();
+		}
+		else throw new error;
 	}
 
 	size() {
-
+		return this.heap.size();
 	}
 
 	isEmpty() {
-		
+		return this.heap.isEmpty();
 	}
 }
 
